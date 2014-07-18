46eceff5382c519ff2cd8a1786afc8ee19bf4fb0
Dexter <dexterkaupas@gmail.com>
2014-07-18 20:06:00 +0200

    Create User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717180859) do
+ActiveRecord::Schema.define(version: 20140718174229) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -41,17 +41,18 @@ ActiveRecord::Schema.define(version: 20140717180859) do
     t.datetime "updated_at"
   end
 
-  create_table "payment_types", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
